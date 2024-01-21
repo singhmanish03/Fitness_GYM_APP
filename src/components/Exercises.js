2915b86381e7e4cade0356d30d356807cdfe5183
singhmanish03 <mailsinghmanish03@gmail.com>
2024-01-22 01:11:31 +0530

    Fitness app pushed

---
+++ src/components/Exercises.js
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from 'react';
+import Pagination from '@mui/material/Pagination';
+import { Box, Stack, Typography } from '@mui/material';
+
+import { exerciseOptions, fetchData } from '../utils/fetchData';
+import ExerciseCard from './ExerciseCard';
+import Loader from './Loader';
+
+const Exercises = ({ exercises, setExercises, bodyPart }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [exercisesPerPage] = useState(6);
+
+   useEffect(() => {
+    const fetchExercisesData = async () => {
+      let exercisesData = [];
+
+      if (bodyPart === 'all') {
+        exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
+      } else {
+        exercisesData = await fetchData(`https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`, exerciseOptions);
+      }
+
+      setExercises(exercisesData);
+    };
+
+    fetchExercisesData();
+  }, [bodyPart,]);
+
+  // Ensure exercises is an array before slicing
+  const currentExercises = Array.isArray(exercises) ? exercises.slice(
+    (currentPage - 1) * exercisesPerPage,
+    currentPage * exercisesPerPage
+  ) : [];
+
+  const paginate = (event, value) => {
+    setCurrentPage(value);
+
+    // You can optionally scroll here, but ensure that it's within bounds
+    // window.scrollTo({ top: 1800, behavior: 'smooth' });
+  };
+
+  if (!Array.isArray(currentExercises) || currentExercises.length === 0) {
+    return <Loader />;
+  }
+
+  return (
+    <Box id="exercises" sx={{ mt: { lg: '109px' } }} mt="50px" p="20px">
+      <Typography variant="h4" fontWeight="bold" sx={{ fontSize: { lg: '44px', xs: '30px' } }} mb="46px">Showing Results</Typography>
+      <Stack direction="row" sx={{ gap: { lg: '107px', xs: '50px' } }} flexWrap="wrap" justifyContent="center">
+        {currentExercises.map((exercise, idx) => (
+          <ExerciseCard key={idx} exercise={exercise} />
+        ))}
+      </Stack>
+      <Stack sx={{ mt: { lg: '114px', xs: '70px' } }} alignItems="center">
+        {exercises.length > 9 && (
+          <Pagination
+            color="standard"
+            shape="rounded"
+            // color="primary"
+            defaultPage={1}
+            count={Math.ceil(exercises.length / exercisesPerPage)}
+            page={currentPage}
+            onChange={paginate}
+            size="large"
+          />
+        )}
+      </Stack>
+    </Box>
+  );
+};
+
+export default Exercises;
